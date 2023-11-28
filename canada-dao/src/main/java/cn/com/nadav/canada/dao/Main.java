02900cbf887209ee0727c9318c2c5f1aed3e8e9a
nadav <nadav.cheung@gmail.com>
2023-11-28 15:32:18 +0800

    project init commit

---
+++ canada-dao/src/main/java/cn/com/nadav/canada/dao/Main.java
@@ -0,0 +1,11 @@
+package cn.com.nadav.canada.dao;
+
+/**
+ * @author cheung nadav
+ * @date 7/27/23
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ canada-web/src/test/java/cn/com/nadav/canada/CanadaApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.com.nadav.canada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanadaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
